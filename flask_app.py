d5010af00512fdb0afc6512ec6c9d81ba473fd3d
sharuhinda <sharuhinda@gmail.com>
2022-11-25 16:56:43 +0300

    get rid of long comment

--- flask_app.py
+++ flask_app.py
@@ -6,17 +6,16 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-"""
-Toy data for model is taken from sklearn 'diabetes' dataset (excluded s1, s2, s4-s6 features)
-Final predictors are: ['age', 'sex', 'bmi', 'bp', 's3'] where 'bmi' - body mass index, 'bp' - average blood pressure, 's3' - blood serum measurement
-Model trained: 
-                XGBRegressor(
-                        learning_rate=0.1, n_estimators=140, max_depth=3,
-                        min_child_weight=3, gamma=0, subsample=0.8, colsample_bytree=0.8,
-                        objective= 'reg:squarederror', nthread=4, scale_pos_weight=1, seed=27
-                    )
-
-"""
+#"""
+#Toy data for model is taken from sklearn 'diabetes' dataset (excluded s1, s2, s4-s6 features)
+#Final predictors are: ['age', 'sex', 'bmi', 'bp', 's3'] where 'bmi' - body mass index, 'bp' - average blood pressure, 's3' - blood serum measurement
+#Model trained: 
+#                XGBRegressor(
+#                        learning_rate=0.1, n_estimators=140, max_depth=3,
+#                        min_child_weight=3, gamma=0, subsample=0.8, colsample_bytree=0.8,
+#                        objective= 'reg:squarederror', nthread=4, scale_pos_weight=1, seed=27
+#                    )
+#"""
 
 model = pickle.load(open('models/xgb_v1.pkl', 'rb'))
 
